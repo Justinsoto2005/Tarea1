edd8e6064926cf99bd2b4861ec64962ec537ed52
Geova11 <geojimen@espol.edu.ec>
2025-11-01 22:48:50 -0500

    Create ControlCalidad.java

---
+++ tarea1/src/main/java/com/tarea1/ControlCalidad.java
@@ -0,0 +1,11 @@
+package com.tarea1;
+
+public class ControlCalidad {
+    public void revisarPronostico(Pronostico pronostico){
+        if (pronostico.categoria.validarPronostico(pronostico)){
+            System.out.println("Pronóstico válido");
+        }else{
+            System.out.println("Pronóstico inválido");
+        }
+    }
+}
