cf23f775dde250034d658278dc85b753fe480eaa
Geova11 <geojimen@espol.edu.ec>
2025-11-01 22:47:36 -0500

    Create CategoriaPronostico.java
    CatePro

---
+++ tarea1/src/main/java/com/tarea1/CategoriaPronostico.java
@@ -0,0 +1,10 @@
+package com.tarea1;
+public class CategoriaPronostico {
+    String nombre;
+    String descripcion;
+
+    public boolean validarPronostico(Pronostico pronostico){
+        return pronostico != null && pronostico.prediccion != null && !pronostico.prediccion.isEmpty();
+    }
+
+}
