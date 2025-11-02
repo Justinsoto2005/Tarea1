ddc47727369b132db306c316a9ca25058aa0bb50
Justinsoto2005 <justinsotoramos2005@gmail.com>
2025-11-01 20:51:41 -0500

    Correccion
    Se corrigio ciertas variables

--- Tarea1/src/tarea1/Apostador.java
+++ Tarea1/src/tarea1/Apostador.java
@@ -2,13 +2,11 @@ package tarea1;
 public class Apostador extends Usuario {
     private int saldoPuntos;
     private int ranking;
-    private List<Pronostico> pronosticos;
 
     public Apostador(String idUsuario, String nombre, String email) {
         super(idUsuario, nombre, email);
         this.saldoPuntos = 0;
         this.ranking = 0;
-        this.pronosticos = new ArrayList<>();
     }
 
     public Pronostico realizarPronostico() {
@@ -16,6 +14,6 @@ public class Apostador extends Usuario {
     }
 
     public void canjearPuntos() {
-        
+
     }
 }
